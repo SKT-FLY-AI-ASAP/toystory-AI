304a843098c418e7db151a35c6eec268ab82f118
Yusun Choi <nsuy.ch@gmail.com>
2024-09-01 16:10:23 +0900

    feat: Modify content API

--- toystory_AI/main.py
+++ toystory_AI/main.py
@@ -8,15 +8,18 @@ import os
 import logging
 import httpx
 from datetime import datetime
+from dotenv import load_dotenv
+from io import BytesIO
 
+load_dotenv()
 app = FastAPI()
 
 # 로깅 설정
 logging.basicConfig(level=logging.INFO)
 
 class ContentRequest(BaseModel):
-    user_id: int = None
-    title: str = None
+    user_id: int
+    title: str
     image_url: str = None
     prompt: str = None
 
@@ -27,40 +30,37 @@ class GeneratedContent(BaseModel):
     stl_url: str = None
     mp3_url: str = None
 
+class S3ResponseData(BaseModel):
+    url: str
+
 class S3Response(BaseModel):
-    url: str = None
+    data: S3ResponseData
 
-# 파일 업로드 엔드포인트
-@app.post("/uploadfile/")
-async def upload_file(file: UploadFile = File(...)):
-    try:
-        # 파일을 /tmp/ 경로에 저장
-        file_path = f"/tmp/{file.filename}"
+async def add_to_s3(file: BytesIO, object_name: str, content_type: str):
+    BASE_URL = os.getenv('SERVER_BASE_URL')
+    SERVER_URL = f"{BASE_URL}/api/v1/s3"
 
-        with open(file_path, "wb") as buffer:
-            shutil.copyfileobj(file.file, buffer)
-
-        logging.info(f"File saved at {file_path}")
-        return {"file_path": file_path}
-
-    except Exception as e:
-        logging.error(f"Failed to save file: {str(e)}")
-        raise HTTPException(status_code=500, detail="File upload failed")
-
-async def add_to_s3(file: UploadFile, object_name: str):
-    SERVER_URL = os.getenv("SERVER_BASE_URL") + "/api/v1/s3"
-    files = {"file": file}
-    metadata = {"object_name": object_name}
+    # Multipart-form data for http
+    files = {
+        "file": (object_name, file, content_type),
+        "object_name": (None, object_name),
+        "content_type": (None, content_type)
+    }
 
     async with httpx.AsyncClient() as cli:
-        response = await cli.post(SERVER_URL, files=files, data=metadata)
-        data = S3Response(**response.json())
-
-        return data.url
+        try:
+            response = await cli.post(SERVER_URL, files=files)
+            res_data = response.json()
+            res = S3Response(**res_data)
+            print("res", res.data.url)
+            return res.data.url
+        except Exception as e:
+            logging.error(str(e))
+            raise HTTPException(status_code=500, detail=str(e))
 
 # 모델 생성 엔드포인트
 @app.post("/api/v1/model", response_model=GeneratedContent, status_code=201)
-def generate_content(content_req: ContentRequest):
+async def generate_content(content_req: ContentRequest):
     try:
         # 이미지 URL 또는 프롬프트를 사용하여 콘텐츠 생성
         if content_req.prompt:
@@ -87,8 +87,22 @@ def generate_content(content_req: ContentRequest):
         res = {}
         for file_path in [png_path, bg_png_path, glb_path, stl_path, mp3_path]:
             with open(file_path, 'rb') as file:
+                file_content = file.read()
+                file_content_io = BytesIO(file_content)
+
+                ext = (file_path.split(".")[-1]).lower()
+                if 'png' in ext:
+                    content_type = 'image/png'
+                elif ('jpg' in ext) or ('jpeg' in ext):
+                    content_type = 'image/jpeg'
+                elif 'mp3' in ext:
+                    content_type = 'audio/mpeg'
+                else:
+                    content_type = 'application/octet-stream'
+
                 current_time = datetime.now().strftime('%Y%m%dT%H:%M:%S')
-                url = add_to_s3(file=file, object_name=f'{current_time}.{os.path.basename(file_path)}')
+                url = await add_to_s3(file=file_content_io, object_name=f'{content_req.user_id}-{current_time}-{(os.path.basename(file_path)).replace(" ", "")}', content_type=content_type)
+                print(url)
                 key = key_mapping.get(file_path, file_path)  # 기본적으로 file_path를 사용
                 res[key] = url
 
